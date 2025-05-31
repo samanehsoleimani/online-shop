81af090eaf362c2c7a4d3a0f9fa3f18918cc53cd
samanehsoleimani <soleimanisaman08@gmail.com>
2025-05-31 11:48:24 +0430

    aadmin & user blueprints added

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from blueprints.general import app as general
+from blueprints.admin import app as admin
+from blueprints.user import app as user
+
+
+
+
+
 """"Appاولی برای بلوپرینت دومی برای کل برنامه"""
 app = Flask(__name__)
 app.register_blueprint(general)
+app.register_blueprint(user)
+app.register_blueprint(admin)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ blueprints/user.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+
+app = Blueprint("user" ,__name__)
+
+
+@app.route('/user')
+def user():
+    return "this is user page"
+
+
+
