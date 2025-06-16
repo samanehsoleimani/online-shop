e216b6f47ce39d40e46c024549df8bed7b0c2800
samanehsoleimani <soleimanisaman08@gmail.com>
2025-06-16 18:39:01 +0430

    init project structure

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask
+
+import config
 from blueprints.general import app as general
 from blueprints.admin import app as admin
 from blueprints.user import app as user
+import config
+import extentions
 
-
-
-
-
-""""Appاولی برای بلوپرینت دومی برای کل برنامه"""
+# اپلیکیشن اصلی و ثبت بلوپرینت‌ها
 app = Flask(__name__)
 app.register_blueprint(general)
 app.register_blueprint(user)
 app.register_blueprint(admin)
 
+# تنظیمات دیتابیس و راه‌اندازی
+app.config["SQLALCHEMY_DATABASE_URI"] =config.SQLALCHEMY_DATABASE_URI
+extentions.db.init_app(app)
+
+with app.app_context():
+    extentions.db.create_all()
 
 if __name__ == '__main__':
     app.run()
