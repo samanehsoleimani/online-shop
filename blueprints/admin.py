60a8e48f95294b17182b33f9221e70e6ad15f35e
samanehsoleimani <soleimanisaman08@gmail.com>
2025-07-11 11:02:17 +0430

    تکمیل بخش افزودن محصول

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,8 +1,11 @@
+from pydoc import describe
+
 from flask import Blueprint, render_template, request, session, abort, url_for
 from werkzeug.utils import redirect
 import os
 import config
-
+from models.product import Product
+from extentions import db
 admin = Blueprint(
     'admin',
     __name__,
@@ -32,6 +35,23 @@ def login():
 def dashboard():
     return render_template('dashboard.html')
 
-@admin.route('/admin/dashboard/products', methods=['GET'])
+@admin.route('/admin/dashboard/products', methods=['GET' ,'POST'])
 def products():
-    return render_template('products.html')
+    if request.method == "GET":
+        products=Product.query.all()
+        return render_template('products.html',products=products)
+    else:
+        name = request.form.get('name',None)
+        price = request.form.get('price',None)
+        description = request.form.get('description',None)
+        active =request.form.get('active',None)
+
+        p = Product( name=name ,description=description,price=price)
+        if active == None:
+            p.active = 0
+        else:
+             p.active = 1
+        db.session.add(p)
+        db.session.commit()
+
+        return "done"
