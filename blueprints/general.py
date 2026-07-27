85976145334ad32117b9ad8d6da72d9997626f6b
samanehsoleimani <soleimanisaman08@gmail.com>
2026-07-27 13:07:38 +0430

    ادامه ایجاد صفحه اصلی سایت

--- blueprints/general.py
+++ blueprints/general.py
@@ -9,11 +9,14 @@ app = Blueprint("app" ,__name__)
 
 @app.route('/')
 def home():
-    products = Product.query.all()
+    products = Product.query.filter(Product.active==1).all()
     return render_template("main.html" ,  products=products)
 
 
-
+@app.route('/product/<int:id>/<name>')
+def product(id, name):
+    product = Product.query.filter(Product.id == id ).filter(Product.name == name).filter(Product.active==1).first_or_404()
+    return render_template("product.html" , product=product)
 @app.route('/about')
 def about():
     return render_template("/about.html")
