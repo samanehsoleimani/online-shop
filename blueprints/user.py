b4b44c4dc88e7b36489ce893997d1c4579369bf3
samanehsoleimani <soleimanisaman08@gmail.com>
2026-08-01 12:32:04 +0430

    اصلاحات پنل کاربری و ورود و ثبت نام

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for,flash
 from flask_login import login_user
 from werkzeug.utils import redirect
 
-import models.user
+from models.user import User
 from passlib.hash import sha256_crypt
 
 from extentions import db
@@ -16,15 +16,37 @@ def user():
         return render_template("user/login.html")
     else:
         register = request.form.get('register' ,None)
-        username=request.form.get('username')
+        username=request.form.get('username', None)
         password=request.form.get('password')
-        phone=request.form.get('phone')
-        address=request.form.get('address')
+        phone=request.form.get('phone', None)
+        address=request.form.get('address', None)
 
         if register != None:
-            user= models.user.User(username=username, password= sha256_crypt.encrypt(password), phone= phone, address=address)
-            db.session.add(user)
+            # حالا مستقیماً از User استفاده کن
+            new_user = User(
+                username=username,
+                password=sha256_crypt.encrypt(password),
+                phone=phone,
+                address=address
+            )
+
+            db.session.add(new_user)
             db.session.commit()
-            login_user(user)
+            login_user(new_user)
             return redirect('/user/dashboard')
+
+
+        else:
+            user= User.query.filter(User.username == username).first()
+            if user == None:
+                flash('نام کاربری یا رمز اشتباه است .')
+                return redirect(url_for('user.user'))
+
+            if sha256_crypt.verify(password,user.password):
+                login_user(user)
+                return redirect('/user/dashboard')
+            else:
+                flash('نام کاربری یا رمز اشتباه سات .')
+                return redirect(url_for('user.login'))
+
         return  'done'
